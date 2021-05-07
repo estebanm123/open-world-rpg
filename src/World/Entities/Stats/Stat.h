880e2225622667190246662a3e92765b59e69c12
Esteban <estebanmacaron@yahoo.fr>
2021-05-06 21:38:51 -0700

    Start adding Stat system

---
+++ src/World/Entities/Stats/Stat.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+class Stat {
+public:
+    Stat();
+};
+
+
+
+
---
+++ src/World/Entities/Stats/StatTypes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <string>
+
+struct StatType {};
+
+struct StatTypes {
+    constexpr static StatType Stamina {}; // Affects animation speed, movement speed, rotation speed
+};
+
+
+
+
